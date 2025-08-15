f355a7dcc9f15e131f2655b18caed3664559b131
luisandrade <l.andrade@canalcero.com>
2025-08-15 07:06:54 -0400

    fix codigo borrado en encoding save

--- files/models.py
+++ files/models.py
@@ -1583,12 +1583,167 @@ def media_m2m(sender, instance, **kwargs):
 def encoding_file_save(sender, instance, created, **kwargs):
     """Performs actions on encoding file save."""
 
-    if instance.status == "success" and not instance.chunk and instance.profile.extension == "mp4":
-        # Cambiar el estado del Media a "waiting_smil"
+    if instance.chunk and instance.status == "success":
+
         instance.media.encoding_status = "waiting_smil"
         instance.media.save(update_fields=["encoding_status"])
 
         # Intentar generar el SMIL
         generate_smil(instance.media)
+
+        # a chunk got completed
+        # check if all chunks are OK
+        # then concatenate to new Encoding - and remove chunks
+        # this should run only once!
+        if instance.media_file:
+            try:
+                orig_chunks = json.loads(instance.chunks_info).keys()
+            except BaseException:
+                instance.delete()
+                return False
+
+            chunks = Encoding.objects.filter(
+                media=instance.media,
+                profile=instance.profile,
+                chunks_info=instance.chunks_info,
+                chunk=True,
+            ).order_by("add_date")
+
+            complete = True
+
+            # perform validation, make sure everything is there
+            for chunk in orig_chunks:
+                if not chunks.filter(chunk_file_path=chunk):
+                    complete = False
+                    break
+
+            for chunk in chunks:
+                if not (chunk.media_file and chunk.media_file.path):
+                    complete = False
+                    break
+
+            if complete:
+                # concatenate chunks and create final encoding file
+                chunks_paths = [f.media_file.path for f in chunks]
+
+                with tempfile.TemporaryDirectory(dir=settings.TEMP_DIRECTORY) as temp_dir:
+                    seg_file = helpers.create_temp_file(suffix=".txt", dir=temp_dir)
+                    tf = helpers.create_temp_file(suffix=".{0}".format(instance.profile.extension), dir=temp_dir)
+                    with open(seg_file, "w") as ff:
+                        for f in chunks_paths:
+                            ff.write("file {}\n".format(f))
+                    cmd = [
+                        settings.FFMPEG_COMMAND,
+                        "-y",
+                        "-f",
+                        "concat",
+                        "-safe",
+                        "0",
+                        "-i",
+                        seg_file,
+                        "-c",
+                        "copy",
+                        "-pix_fmt",
+                        "yuv420p",
+                        "-movflags",
+                        "faststart",
+                        tf,
+                    ]
+                    stdout = helpers.run_command(cmd)
+
+                    encoding = Encoding(
+                        media=instance.media,
+                        profile=instance.profile,
+                        status="success",
+                        progress=100,
+                    )
+                    all_logs = "\n".join([st.logs for st in chunks])
+                    encoding.logs = "{0}\n{1}\n{2}".format(chunks_paths, stdout, all_logs)
+                    workers = list(set([st.worker for st in chunks]))
+                    encoding.worker = json.dumps({"workers": workers})
+
+                    start_date = min([st.add_date for st in chunks])
+                    end_date = max([st.update_date for st in chunks])
+                    encoding.total_run_time = (end_date - start_date).seconds
+                    encoding.save()
+
+                    with open(tf, "rb") as f:
+                        myfile = File(f)
+                        output_name = "{0}.{1}".format(
+                            helpers.get_file_name(instance.media.media_file.path),
+                            instance.profile.extension,
+                        )
+                        encoding.media_file.save(content=myfile, name=output_name)
+
+                    # encoding is saved, deleting chunks
+                    # and any other encoding that might exist
+                    # first perform one last validation
+                    # to avoid that this is run twice
+                    if (
+                        len(orig_chunks)
+                        == Encoding.objects.filter(  # noqa
+                            media=instance.media,
+                            profile=instance.profile,
+                            chunks_info=instance.chunks_info,
+                        ).count()
+                    ):
+                        # if two chunks are finished at the same time, this
+                        # will be changed
+                        who = Encoding.objects.filter(media=encoding.media, profile=encoding.profile).exclude(id=encoding.id)
+                        who.delete()
+                    else:
+                        encoding.delete()
+                    if not Encoding.objects.filter(chunks_info=instance.chunks_info):
+                        # TODO: in case of remote workers, files should be deleted
+                        # example
+                        # for worker in workers:
+                        #    for chunk in json.loads(instance.chunks_info).keys():
+                        #        remove_media_file.delay(media_file=chunk)
+                        for chunk in json.loads(instance.chunks_info).keys():
+                            helpers.rm_file(chunk)
+                    instance.media.post_encode_actions(encoding=instance, action="add")
+
+    elif instance.chunk and instance.status == "fail":
+        encoding = Encoding(media=instance.media, profile=instance.profile, status="fail", progress=100)
+
+        chunks = Encoding.objects.filter(media=instance.media, chunks_info=instance.chunks_info, chunk=True).order_by("add_date")
+
+        chunks_paths = [f.media_file.path for f in chunks]
+
+        all_logs = "\n".join([st.logs for st in chunks])
+        encoding.logs = "{0}\n{1}".format(chunks_paths, all_logs)
+        workers = list(set([st.worker for st in chunks]))
+        encoding.worker = json.dumps({"workers": workers})
+        start_date = min([st.add_date for st in chunks])
+        end_date = max([st.update_date for st in chunks])
+        encoding.total_run_time = (end_date - start_date).seconds
+        encoding.save()
+
+        who = Encoding.objects.filter(media=encoding.media, profile=encoding.profile).exclude(id=encoding.id)
+
+        who.delete()
+        # TODO: merge with above if, do not repeat code
+    else:
+        if instance.status in ["fail", "success"]:
+            instance.media.post_encode_actions(encoding=instance, action="add")
+
+        encodings = set([encoding.status for encoding in Encoding.objects.filter(media=instance.media)])
+        if ("running" in encodings) or ("pending" in encodings):
+            return
+
+
+@receiver(post_delete, sender=Encoding)
+def encoding_file_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `Encoding` object is deleted.
+    """
+
+    if instance.media_file:
+        helpers.rm_file(instance.media_file.path)
+        if not instance.chunk:
+            instance.media.post_encode_actions(encoding=instance, action="delete")
+    # delete local chunks, and remote chunks + media file. Only when the
+    # last encoding of a media is complete
 
 
